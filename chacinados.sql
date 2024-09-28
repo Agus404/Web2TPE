eeb78411eb2747dab3535db1c7d79c331f92052b
lAu-201 <lautaroa201@gmail.com>
2024-09-28 19:51:49 -0300

    Eliminado tabla categoria

--- chacinados.sql
+++ chacinados.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2024 a las 23:38:20
+-- Tiempo de generación: 29-09-2024 a las 00:51:00
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,27 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `categorias`
---
-
-CREATE TABLE `categorias` (
-  `id_categoria` int(11) NOT NULL,
-  `nombre_categoria` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `categorias`
---
-
-INSERT INTO `categorias` (`id_categoria`, `nombre_categoria`) VALUES
-(1, 'Bondiola'),
-(2, 'Salamin'),
-(5, 'Jamon'),
-(6, 'Lomo');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `marcas`
 --
 
@@ -61,7 +40,8 @@ CREATE TABLE `marcas` (
 
 INSERT INTO `marcas` (`id_marca`, `nombre_marca`, `contacto`, `sede`) VALUES
 (1, 'Cagnoli', 'info@cagnoli.com', 'Sección Chacras 43, Tandil, Argentina.'),
-(2, 'Las Dinas', 'dinas.salumeria@hotmail.com', 'Parque Industrial, Tandil, Argentina.');
+(2, 'Las Dinas', 'dinas.salumeria@hotmail.com', 'Parque Industrial, Tandil, Argentina.'),
+(5, 'Paladini', 'info@paladini.com', 'Carlos Tejedor 2040, Cordoba, Argentina');
 
 -- --------------------------------------------------------
 
@@ -74,29 +54,23 @@ CREATE TABLE `productos` (
   `nombre_producto` varchar(50) NOT NULL,
   `peso` int(11) NOT NULL,
   `precio` decimal(10,2) NOT NULL,
-  `id_marca` int(11) NOT NULL,
-  `id_categoria` int(11) NOT NULL
+  `categoria` varchar(50) NOT NULL,
+  `id_marca` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id_producto`, `nombre_producto`, `peso`, `precio`, `id_marca`, `id_categoria`) VALUES
-(1, 'Bondiola ahumada', 200, 7000.00, 2, 1),
-(2, 'Salamin picado grueso', 150, 4660.00, 1, 2),
-(3, 'Jamon crudo tipo Westfalia', 200, 8600.00, 2, 5),
-(4, 'Salamin picado fino', 130, 4500.00, 1, 2);
+INSERT INTO `productos` (`id_producto`, `nombre_producto`, `peso`, `precio`, `categoria`, `id_marca`) VALUES
+(5, 'Bondiola ahumada', 200, 7000.00, 'Bondiola', 2),
+(6, 'Salamin picado grueso', 150, 4660.00, 'Salamin', 1),
+(7, 'Jamon cocido horneado', 400, 99999.99, 'Jamon', 5),
+(8, 'Longaniza calabresa', 200, 4500.00, 'Salamin', 2);
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id_categoria`);
 
 --
 -- Indices de la tabla `marcas`
@@ -110,30 +84,23 @@ ALTER TABLE `marcas`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `id_marca` (`id_marca`),
-  ADD KEY `categoria` (`id_categoria`);
+  ADD KEY `id_marca` (`id_marca`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- AUTO_INCREMENT de la tabla `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
@@ -143,8 +110,7 @@ ALTER TABLE `productos`
 -- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id_marca`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id_marca`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
